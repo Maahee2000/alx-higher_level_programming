01a2f5dc9052f434946088dd0d41e80801fff324
maahee200 <yongzmahi99@gmail.com>
2022-07-02 17:22:22 -0700

    module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """HAndle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
     import sys
-
-    if len(sys.srgv) - 1 != 3:
-        print("usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-        ops = {"+": add, "-": sub, "*" : mul, "/": div}
-        if sys.argv[2]not in list(ops.key()):
-            print("Unknown operator. Available operator: +, -, * and/")
-            sys.exit(1)
-
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-            print("{} { {} = {}".format(a, sys.srgv[2], b, ops[sys.argv[2]](a, b)))
+    import sys
+    argc = len(args) - 1
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(argc, "s" if argc > 1 else ""))
+        for i in range(argc):
+            print("{}: {}".format(i + 1, args[i + 1]))
