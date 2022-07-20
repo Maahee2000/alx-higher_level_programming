42e73b9ee5a73b4c32fe29ecedc1050be5d53404
maahee200 <yongzmahi99@gmail.com>
2022-07-19 19:00:30 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            real_num += 1
+            ret += 1
         except (IndexError, ValueError, TypeError):
             break
         print()
